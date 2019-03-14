f51fe20ce384b942657adab40de23ac5ca07dbd9
Akshit Grover <akshit.grover2016@gmail.com>
2019-03-14 18:50:08 +0530

    Server -> SocketController: Fix unauthorized socket disconnect bug

--- server/src/controllers/socket.ts
+++ server/src/controllers/socket.ts
@@ -1,8 +1,10 @@
 import { io } from '../app';
 import { fetch, fetchedQuestions } from '../utils/fetchQuestions';
+import * as jwt from '../utils/jwt';
 
 let adminLock = true;
 let adminId: string = null;
+let initialized: boolean = false;
 
 io.on('connection', (socket) => {
   socket.on('adminLogin', (password, cb): void => {
@@ -13,12 +15,24 @@ io.on('connection', (socket) => {
       cb(true);
       return null;
     }
+    socket.disconnect();
     cb(false);
     return null;
   });
   socket.on('emitQuestion', (): void => {
     if (socket.id === adminId) {
-      socket.emit('question', fetchedQuestions[0]);
+      io.emit('question', fetchedQuestions[0]);
+      initialized = true;
+    }
+  });
+  socket.on('join', (token, cb): void => {
+    let payload: any;
+    try {
+      payload = jwt.verify(token);
+      console.log(payload);
+    } catch (err) {
+      cb(false);
+      socket.disconnect();
     }
   });
   socket.on('disconnect', () => {
