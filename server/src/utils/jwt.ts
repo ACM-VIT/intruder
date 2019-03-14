285a0a7adce765f6531501ca3cf11d86646531e7
Akshit Grover <akshit.grover2016@gmail.com>
2019-03-14 19:38:19 +0530

    Server -> utils: Fix env var typo

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 
 export function issueToken(payload): string {
-  return jwt.sign(payload, process.env.JWT_SECCRET);
+  return jwt.sign(payload, process.env.JWT_SECRET);
 }
 
 export function verify(token): any {
