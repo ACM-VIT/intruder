9f839c4dbb2198a9325daf2996318a91b56836be
Shubham Awasthi <aw.shubh@gmail.com>
2019-03-23 11:55:30 +0530

    client -> changed base address

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
 // export default 'http://events.acmvit.in/api-intruder'
-export default 'https://cluster.acmvit.in:30001'
+export default 'http://cluster.acmvit.in:30001'
 // export default 'http://192.168.43.126:5000'
 // export default 'http://35.244.18.126/'
